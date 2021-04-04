7de629585c234b518b4309105d6a2f72507deb18
Leptopoda <rimikis.nikolas@gmail.com>
2021-04-04 16:08:32 +0200

    fix listview

--- lib/screens/bierverkostung/new_tasting.dart
+++ lib/screens/bierverkostung/new_tasting.dart
@@ -24,15 +24,39 @@ class NewTasting extends StatefulWidget {
 
 class _NewTastingState extends State<NewTasting> {
   late DateTime _selectedDate;
-  final TextEditingController _dateController = TextEditingController();
-
-  static const TextStyle _heading =
-      TextStyle(fontSize: 22, color: Colors.yellow);
-  static const TextStyle _text = TextStyle(fontSize: 18);
-
-  final Tasting _tasting = Tasting();
 
   final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _location = TextEditingController();
+  final TextEditingController _beer = TextEditingController();
+  final TextEditingController _beerColour = TextEditingController();
+  final TextEditingController _beerColourDesc = TextEditingController();
+  final TextEditingController _clarity = TextEditingController();
+  final TextEditingController _foamColour = TextEditingController();
+  final TextEditingController _foamStructure = TextEditingController();
+  final TextEditingController _mouthFeelDesc = TextEditingController();
+  final TextEditingController _bodyDesc = TextEditingController();
+  final TextEditingController _aftertasteDesc = TextEditingController();
+  final TextEditingController _foodRecommendation = TextEditingController();
+  final TextEditingController _totalImpressionDesc = TextEditingController();
+
+  static const TextStyle _heading = TextStyle(
+    fontSize: 22,
+    color: Colors.yellow,
+  );
+  static const TextStyle _text = TextStyle(
+    fontSize: 18,
+  );
+
+  int _colourEbc = 4;
+  int _foamStability = 1;
+  int _bitternessRating = 1;
+  int _sweetnessRating = 1;
+  int _acidityRating = 1;
+  int _fullBodiedRating = 1;
+  int _aftertasteRating = 1;
+  int _totalImpressionRating = 1;
 
   @override
   void initState() {
@@ -44,6 +68,18 @@ class _NewTastingState extends State<NewTasting> {
   @override
   void dispose() {
     _dateController.dispose();
+    _location.dispose();
+    _beer.dispose();
+    _beerColour.dispose();
+    _beerColourDesc.dispose();
+    _clarity.dispose();
+    _foamColour.dispose();
+    _foamStructure.dispose();
+    _mouthFeelDesc.dispose();
+    _bodyDesc.dispose();
+    _aftertasteDesc.dispose();
+    _foodRecommendation.dispose();
+    _totalImpressionDesc.dispose();
     super.dispose();
   }
 
@@ -74,23 +110,18 @@ class _NewTastingState extends State<NewTasting> {
             ),
             TextFormField(
               style: _text,
+              controller: _location,
               decoration: const InputDecoration(
                 labelText: 'Location',
                 suffixIcon: Icon(Icons.location_on_outlined),
               ),
-              onSaved: (String? value) => _tasting.location = value,
             ),
             TextFormField(
               style: _text,
+              controller: _beer,
               decoration: const InputDecoration(
                 labelText: 'Bier',
               ),
-              onSaved: (String? value) {
-                final Beer _bier1 = Beer(
-                  beerName: value!,
-                );
-                _tasting.beer = _bier1;
-              },
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Pflichtfeld';
@@ -104,31 +135,31 @@ class _NewTastingState extends State<NewTasting> {
             ),
             TextFormField(
               style: _text,
+              controller: _foamColour,
               decoration: const InputDecoration(
                 labelText: 'Foam Colour',
                 suffixIcon: Icon(Icons.color_lens_outlined),
               ),
-              onSaved: (String? value) => _tasting.foamColour = value,
             ),
             TextFormField(
               style: _text,
+              controller: _foamStructure,
               decoration: const InputDecoration(
                 labelText: 'Foam Structure',
               ),
-              onSaved: (String? value) => _tasting.foamStructure = value,
             ),
             const Text('Foam Stability'),
             Slider(
-              value: _tasting.foamStability.toDouble(),
+              value: _foamStability.toDouble(),
               min: 1,
               max: 4,
               divisions: 3,
-              label: '${_tasting.foamStability}',
+              label: '$_foamStability',
               onChanged: (double value) =>
-                  setState(() => _tasting.foamStability = value.round()),
+                  setState(() => _foamStability = value.round()),
             ),
             DropdownButtonFormField(
-              value: _tasting.colourEbc,
+              value: _colourEbc,
               items: <int>[
                 4,
                 6,
@@ -151,7 +182,7 @@ class _NewTastingState extends State<NewTasting> {
                   );
                 },
               ).toList(),
-              onChanged: (int? val) => setState(() => _tasting.colourEbc = val!),
+              onChanged: (int? val) => setState(() => _colourEbc = val!),
               decoration: const InputDecoration(
                 labelText: 'EBC',
                 // icon: Icon(Icons.calendar_today),
@@ -159,24 +190,24 @@ class _NewTastingState extends State<NewTasting> {
             ),
             TextFormField(
               style: _text,
+              controller: _beerColour,
               decoration: const InputDecoration(
                 labelText: 'Beer Colour',
               ),
-              onSaved: (String? value) => _tasting.beerColour = value,
             ),
             TextFormField(
               style: _text,
+              controller: _beerColourDesc,
               decoration: const InputDecoration(
                 labelText: 'Color Description',
               ),
-              onSaved: (String? value) => _tasting.beerColourDesc = value,
             ),
             TextFormField(
               style: _text,
+              controller: _clarity,
               decoration: const InputDecoration(
                 labelText: 'Clarity',
               ),
-              onSaved: (String? value) => _tasting.clarity = value,
             ),
             const Text(
               'Taste',
@@ -184,72 +215,72 @@ class _NewTastingState extends State<NewTasting> {
             ),
             TextFormField(
               style: _text,
+              controller: _mouthFeelDesc,
               decoration: const InputDecoration(
                 labelText: 'Mundgefühl',
               ),
-              onSaved: (String? value) => _tasting.mouthFeelDesc = value,
             ),
             const Text('Bitterness'),
             Slider(
-              value: _tasting.bitternessRating.toDouble(),
+              value: _bitternessRating.toDouble(),
               min: 1,
               max: 4,
               divisions: 3,
-              label: '${_tasting.bitternessRating}',
+              label: '$_bitternessRating',
               onChanged: (double value) =>
-                  setState(() => _tasting.bitternessRating = value.round()),
+                  setState(() => _bitternessRating = value.round()),
             ),
             const Text('Sweetness'),
             Slider(
-              value: _tasting.sweetnessRating.toDouble(),
+              value: _sweetnessRating.toDouble(),
               min: 1,
               max: 4,
               divisions: 3,
-              label: '${_tasting.sweetnessRating}',
+              label: '$_sweetnessRating',
               onChanged: (double value) =>
-                  setState(() => _tasting.sweetnessRating = value.round()),
+                  setState(() => _sweetnessRating = value.round()),
             ),
             const Text('Acidity'),
             Slider(
-              value: _tasting.acidityRating.toDouble(),
+              value: _acidityRating.toDouble(),
               min: 1,
               max: 4,
               divisions: 3,
-              label: '${_tasting.acidityRating}',
+              label: '$_acidityRating',
               onChanged: (double value) =>
-                  setState(() => _tasting.acidityRating = value.round()),
+                  setState(() => _acidityRating = value.round()),
             ),
             const Text('Body Fullness'),
             Slider(
-              value: _tasting.fullBodiedRating.toDouble(),
+              value: _fullBodiedRating.toDouble(),
               min: 1,
               max: 4,
               divisions: 3,
-              label: '${_tasting.fullBodiedRating}',
+              label: '$_fullBodiedRating',
               onChanged: (double value) =>
-                  setState(() => _tasting.fullBodiedRating = value.round()),
+                  setState(() => _fullBodiedRating = value.round()),
             ),
             TextFormField(
               style: _text,
+              controller: _bodyDesc,
               decoration: const InputDecoration(
                 labelText: 'Body Description',
               ),
-              onSaved: (String? value) => _tasting.bodyDesc = value,
             ),
             TextFormField(
               // TODO: add intensity
               style: _text,
+              controller: _aftertasteDesc,
               decoration: const InputDecoration(
                 labelText: 'Nachgeschmack',
               ),
-              onSaved: (String? value) => _tasting.aftertasteDesc = value,
             ),
             TextFormField(
               style: _text,
+              controller: _foodRecommendation,
               decoration: const InputDecoration(
                 labelText: 'Food Recomendation',
               ),
-              onSaved: (String? value) => _tasting.foodRecommendation = value,
             ),
             const Text(
               'Conclusion',
@@ -257,20 +288,20 @@ class _NewTastingState extends State<NewTasting> {
             ),
             TextFormField(
               style: _text,
+              controller: _totalImpressionDesc,
               decoration: const InputDecoration(
                 labelText: 'Total Impression',
               ),
-              onSaved: (String? value) => _tasting.totalImpressionDesc = value,
             ),
             const Text('Total Rating'),
             Slider(
-              value: _tasting.totalImpressionRating.toDouble(),
+              value: _totalImpressionRating.toDouble(),
               min: 1,
-              max: 4,
-              divisions: 3,
-              label: '${_tasting.totalImpressionRating}',
+              max: 3,
+              divisions: 2,
+              label: '$_totalImpressionRating',
               onChanged: (double value) =>
-                  setState(() => _tasting.totalImpressionRating = value.round()),
+                  setState(() => _totalImpressionRating = value.round()),
             ),
             ElevatedButton(
               onPressed: () => _submit(context),
@@ -290,8 +321,36 @@ class _NewTastingState extends State<NewTasting> {
       ScaffoldMessenger.of(context)
           .showSnackBar(const SnackBar(content: Text('Processing Data')));
       _formKey.currentState!.save();
-      _tasting.date = _selectedDate;
-      await DatabaseService(uid: widget.user.uid).saveTasting(_tasting);
+
+      final Beer _bier1 = Beer(
+        beerName: _beer.value.text,
+      );
+
+      final Tasting _tasting1 = Tasting(
+        beer: _bier1,
+        date: _selectedDate,
+        location: _location.value.text,
+        beerColour: _beerColour.value.text,
+        beerColourDesc: _beerColourDesc.value.text,
+        colourEbc: _colourEbc,
+        clarity: _clarity.value.text,
+        foamColour: _foamColour.value.text,
+        foamStructure: _foamStructure.value.text,
+        foamStability: _foamStability,
+        bitternessRating: _bitternessRating,
+        sweetnessRating: _sweetnessRating,
+        acidityRating: _acidityRating,
+        mouthFeelDesc: _mouthFeelDesc.value.text,
+        fullBodiedRating: _fullBodiedRating,
+        bodyDesc: _bodyDesc.value.text,
+        aftertasteDesc: _aftertasteDesc.value.text,
+        aftertasteRating: _aftertasteRating,
+        foodRecommendation: _foodRecommendation.value.text,
+        totalImpressionDesc: _totalImpressionDesc.value.text,
+        totalImpressionRating: _totalImpressionRating,
+      );
+
+      await DatabaseService(uid: widget.user.uid).saveTasting(_tasting1);
 
       Navigator.of(context).pop();
     }
