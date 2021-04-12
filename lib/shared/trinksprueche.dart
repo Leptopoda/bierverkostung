750aa749cd4fdfe2f00c85d41cc4d54deddffa48
Leptopoda <rimikis.nikolas@gmail.com>
2021-04-12 17:16:58 +0200

    fix typo in header

--- lib/shared/trinksprueche.dart
+++ lib/shared/trinksprueche.dart
@@ -1,5 +1,5 @@
 // Copyright 2021 Leptopoda. All rights reserved.
-// Use of this source code is governed by a APACHE-style license that can be
+// Use of this source code is governed by an APACHE-style license that can be
 // found in the LICENSE file.
 
 import 'dart:math' show Random;
