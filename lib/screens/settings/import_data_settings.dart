8f5cf9023ca280f1765034930dc26db078ae7710
Leptopoda <rimikis.nikolas@gmail.com>
2021-04-21 22:37:12 +0200

    add settings

---
+++ lib/screens/settings/import_data_settings.dart
@@ -0,0 +1,20 @@
+// Copyright 2021 Leptopoda. All rights reserved.
+// Use of this source code is governed by an APACHE-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class ImportDataSettings extends StatelessWidget {
+  const ImportDataSettings({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Import Data'),
+        ),
+        body: const Center(
+          child: Text('coming'),
+        ));
+  }
+}
