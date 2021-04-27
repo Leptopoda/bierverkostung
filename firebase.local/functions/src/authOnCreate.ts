213ca02b7b0a32ff3e0790a9197a6fc8b89794df
Leptopoda <rimikis.nikolas@gmail.com>
2021-04-27 10:50:16 +0200

    modularize cloud functions

---
+++ firebase.local/functions/src/authOnCreate.ts
@@ -0,0 +1,24 @@
+// Copyright 2021 Leptopoda. All rights reserved.
+// Use of this source code is governed by an APACHE-style license that can be
+// found in the LICENSE file.
+
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+const authOnCreate = functions.auth.user().onCreate(async (context) => {
+  try {
+    await admin.auth().setCustomUserClaims(context.uid, {
+      group_id: context.uid,
+    });
+
+    console.log(`${context.uid} has been initialized with default group`);
+    return {message:
+      `Success! ${context.uid} has been initialized with group ${context.uid}.`,
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
+});
+
+export {authOnCreate};
