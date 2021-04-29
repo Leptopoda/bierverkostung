78cd96accd7b7aed8d9b882df3d75a2afe097447
Leptopoda <rimikis.nikolas@gmail.com>
2021-04-29 13:12:49 +0200

    adaptive navbar

--- lib/screens/bierverkostung/bierverkostung.dart
+++ lib/screens/bierverkostung/bierverkostung.dart
@@ -10,51 +10,66 @@ import 'package:bierverkostung/models/users.dart';
 import 'package:bierverkostung/shared/error_page.dart';
 import 'package:bierverkostung/models/tastings.dart';
 
+import 'package:bierverkostung/screens/conference/conference.dart';
+import 'package:bierverkostung/screens/promille_rechner/promille_rechner.dart';
+import 'package:bierverkostung/screens/settings/settings_button.dart';
+
 class Bierverkostung extends StatelessWidget {
   const Bierverkostung({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final UserData _user = Provider.of<UserData?>(context)!;
-    return StreamBuilder<List<Tasting>>(
-      stream: DatabaseService(user: _user).tastings,
-      builder: (BuildContext context, AsyncSnapshot<List<Tasting>> snapshot) {
-        if (snapshot.hasError) {
-          return SomethingWentWrong(
-            error: '${snapshot.error}',
-          );
-        }
-
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return const Center(
-              child: CircularProgressIndicator(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bierverkostung'),
+        actions: const <Widget>[
+          MeetingButton(),
+          PromilleRechnerButton(),
+          SettingsButton(),
+        ],
+      ),
+      body: StreamBuilder<List<Tasting>>(
+        stream: DatabaseService(user: _user).tastings,
+        builder: (BuildContext context, AsyncSnapshot<List<Tasting>> snapshot) {
+          if (snapshot.hasError) {
+            return SomethingWentWrong(
+              error: '${snapshot.error}',
             );
-          default:
-            if (!snapshot.hasData) {
+          }
+
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
               return const Center(
-                child: Text('noch keine Verkostungen vorhanden'),
+                child: CircularProgressIndicator(),
               );
-            }
-
-            return ListView.separated(
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
-              padding: const EdgeInsets.all(16.0),
-              itemCount: snapshot.data!.length,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text(
-                    'Bier: ${snapshot.data![index].beer.beerName} Datum: ${snapshot.data![index].date}',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  onTap: () => Navigator.pushNamed(context, '/DispTasting',
-                      arguments: snapshot.data![index]),
+            default:
+              if (!snapshot.hasData) {
+                return const Center(
+                  child: Text('noch keine Verkostungen vorhanden'),
                 );
-              },
-            );
-        }
-      },
+              }
+
+              return ListView.separated(
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+                padding: const EdgeInsets.all(16.0),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(
+                      'Bier: ${snapshot.data![index].beer.beerName} Datum: ${snapshot.data![index].date}',
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                    onTap: () => Navigator.pushNamed(context, '/DispTasting',
+                        arguments: snapshot.data![index]),
+                  );
+                },
+              );
+          }
+        },
+      ),
+      floatingActionButton: const BierverkostungFab(),
     );
   }
 }
--- lib/screens/trinkspiele/trinkspiele.dart
+++ lib/screens/trinkspiele/trinkspiele.dart
@@ -2,7 +2,12 @@
 // Use of this source code is governed by an APACHE-style license that can be
 // found in the LICENSE file.
 
+import 'package:bierverkostung/shared/home_app_bar.dart';
 import 'package:flutter/material.dart';
+
+import 'package:bierverkostung/screens/conference/conference.dart';
+import 'package:bierverkostung/screens/promille_rechner/promille_rechner.dart';
+import 'package:bierverkostung/screens/settings/settings_button.dart';
 
 class Trinkspiele extends StatelessWidget {
   static const List<String> _spiele = [
@@ -18,21 +23,31 @@ class Trinkspiele extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
-      padding: const EdgeInsets.all(16.0),
-      itemCount: _spiele.length,
-      itemBuilder: (BuildContext context, int index) {
-        return ListTile(
-          leading: const Icon(Icons.message_outlined),
-          title: Text(
-            _spiele[index],
-            style: const TextStyle(fontSize: 18),
-          ),
-          trailing: const Icon(Icons.keyboard_arrow_right),
-          onTap: () => Navigator.pushNamed(context, _spielePages[index]),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Trinkspiele'),
+        actions: const <Widget>[
+          MeetingButton(),
+          PromilleRechnerButton(),
+          SettingsButton(),
+        ],
+      ),
+      body: ListView.separated(
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        padding: const EdgeInsets.all(16.0),
+        itemCount: _spiele.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: const Icon(Icons.message_outlined),
+            title: Text(
+              _spiele[index],
+              style: const TextStyle(fontSize: 18),
+            ),
+            trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap: () => Navigator.pushNamed(context, _spielePages[index]),
+          );
+        },
+      ),
     );
   }
 }
