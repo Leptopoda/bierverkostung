5b5377fa370e82335a143270087e119d782acde8
Leptopoda <rimikis.nikolas@gmail.com>
2021-06-03 18:18:57 +0200

    clean up functions

--- firebase.local/functions/src/index.ts
+++ firebase.local/functions/src/index.ts
@@ -2,8 +2,8 @@
 // Use of this source code is governed by an APACHE-style license that can be
 // found in the LICENSE file.
 
-import * as admin from "firebase-admin";
-admin.initializeApp();
+import {initializeApp} from "firebase-admin";
+initializeApp();
 
 export {authOnCreate} from "./authOnCreate";
 export {addGroup} from "./addGroup";
